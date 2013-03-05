f886049cf4b5f5884ed738d8553c6184e1bcf46b
kia <kbahmani@mudynamics.com>
2013-03-05 14:05:38 -0800

    Step-0 : Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestfulUx::Application.config.secret_token = 'b4dca0f7fe48f60dd2fe8f1dafa193e79e7386a841424745db4651100af399b90b00cfb432f7499f5f73eed054812d57bd899b8328163fbebf1d814a04c6ba22'
---
+++ public/canjs/global/main.js
@@ -0,0 +1,59 @@
+steal(
+    'global/lib/jquery-1.7.2.js'
+).then('global/lib/can.jquery-1.0.7.min.js')
+.then(
+    'global/lib/jquery-ui-1.8.15.min.js',
+    'global/lib/jquerypp/jquery.cookie.js',   // load jquery++ cookie
+    'global/lib/jquerypp/jquery.form_params.js',
+    'global/css/jquery-ui-1.8.15.custom.css'
+)
+.then(function(){
+    // restore the globe states from sessionStorage
+
+    if (!window.sessionStorage ) {
+        window.sessionStorage = {}; // make it compatible for non-HTML5 browsers
+    }
+
+    // app_state stores the application states so it can be refered globally
+    if(!window.app_state){
+        var old_state =  window.sessionStorage["app_state"];
+        if(!old_state || old_state.length == 0){
+            old_state = "{}";
+        }
+
+        window.app_state = new can.Observe(JSON.parse(old_state));
+        app_state.bind( 'change', function( ev, attr, how, newVal, oldVal ) {
+           window.sessionStorage["app_state"] = JSON.stringify(window.app_state._data.valueOf());
+        });
+    }
+    // create a route
+    can.route( ':widget/:sub' );
+
+    var Routing = can.Control({
+        ':widget route': function(data) {
+            this.handle_route(data);
+        },
+        ':widget/:sub route': function( data ) {
+            this.handle_route(data);
+        },
+        handle_route: function(data){
+            var widget = data['widget'];
+            var sub = data['sub'];
+            var self = this;
+            app_state.attr('route', {'widget': widget, 'sub': sub});
+        }
+    })
+
+    // create routing control
+    new Routing( document.body );
+
+})
+.then(
+	'global/lib/can.fixture.js',
+	'global/lib/can.construct.proxy.js',
+	'global/lib/can.construct.super.js',
+	'global/lib/can.control.plugin.js',
+	'global/lib/can.control.view.js',
+	'global/lib/can.view.modifiers.js',
+	'global/css/layout.css'
+)
