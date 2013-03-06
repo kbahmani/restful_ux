43fbb2755476200fa69c8179df3299b003366c20
kia <kbahmani@mudynamics.com>
2013-03-05 18:28:10 -0800

    Step-11 : CSS cleanup and final DRY up

--- public/canjs/app/users/users_controller.js
+++ public/canjs/app/users/users_controller.js
@@ -15,53 +15,59 @@ can.Control("App.UsersCtrl",{},
 			}
 		});
 	},
+	remove_user: function(ui, event){
+		var user_row = ui.parent();
+		var user = user_row.data('model');
+		user.destroy(function( user ) {
+		   user_row.remove();
+		});
+	},
+	read_form: function(obj, form){
+		obj['name'] = form.find('.first_name input').val();
+		obj['family'] = form.find('.last_name input').val();
+		obj['email'] = form.find('.email input').val();
+		return obj;
+	},
+	clean_form: function(form){
+		form.find('.first_name input').val('');
+		form.find('.last_name input').val('');
+		form.find('.email input').val('');
+	},
 	'.add_user_btn click': function(ui, event){
 		var panel = this.element.find('.user_list');
 		var User = App.UserModel;
 		var form = ui.parent();
-		var name = form.find('.first_name input').val();
-		var family = form.find('.last_name input').val();
-		var email = form.find('.email input').val();
-		var user = new User( { name: name, family: family, email: email } );
-		form.find('.first_name input').val('');
-		form.find('.last_name input').val('');
-		form.find('.email input').val('');
+		var data = this.read_form({}, form);
+		this.clean_form(form);
+		var user = new User( data );
 		user.save(function( user ) {
 			var html = can.view('../app/users/ejs/user_row.ejs', {data: user});
 			panel.append(html);
 		});
 	},
 	'.ur_delete click': function(ui, event){
-		var user_row = ui.parent();
-		var user = user_row.data('model');
-		user.destroy(function( user ) {
-		   user_row.remove();
-		});
+		this.remove_user(ui, event);
 	},
 	'.ur_edit click': function(ui, event){
 		var user_row = ui.parent();
 		var user = user_row.data('model');
-		user_row.after(can.view('../app/users/ejs/user_form.ejs', {mode: 'edit', data: user}));
+		var html = can.view('../app/users/ejs/user_form.ejs', {mode: 'edit', data: user});
+		user_row.after(html);
 		user_row.remove();
 	},
 	'.eu_delete click': function(ui, event){
-		var user_row = ui.parent();
-		var user = user_row.data('model');
-		user.destroy(function( user ) {
-		   user_row.remove();
-		});
+		this.remove_user(ui, event);
 	},
 	'.eu_save_close click': function(ui, evevnt){
 		var form = ui.parent();
 		var user = form.data('model');
-		user.name = form.find('.first_name input').val();
-		user.family = form.find('.last_name input').val();
-		user.email = form.find('.email input').val();
+		user = this.read_form(user, form);
 		user._data.name  = user.name;
 		user._data.family= user.family;
 		user._data.email = user.email;
 		user.save(function( user ) {
-			form.after(can.view('../app/users/ejs/user_row.ejs', {data: user}));
+			var html = can.view('../app/users/ejs/user_row.ejs', {data: user});
+			form.after(html);
 			form.remove();
 		});
 	}
